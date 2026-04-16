90db609fa9386c2d43ff682fcac4afe90b01e661
Eduardo252 <149748062+Eduardo252@users.noreply.github.com>
2026-04-16 10:15:26 -0600

    Update producto.service.ts

--- inventarios-angular/src/app/producto.service.ts
+++ inventarios-angular/src/app/producto.service.ts
@@ -7,7 +7,7 @@ import { Producto } from './producto';
   providedIn: 'root',
 })
 export class ProductoService {
-  private urlBase = 'http://inventarioapp-production-6dd6.up.railway.app/productos';
+  private urlBase = 'http://inventarioapp-production-6dd6.up.railway.app/inventario-app/productos';
 
   constructor(private clienteHttp: HttpClient) {}
 
